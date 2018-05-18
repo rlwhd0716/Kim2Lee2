f828c6af2f66a9bbb07e2db11b1a9047156dba46
rlwhd0716 <39414359+rlwhd0716@users.noreply.github.com>
2018-05-19 01:36:34 +0900

    first php

---
+++ getjson.php
@@ -0,0 +1,41 @@
+<?php  
+
+$link=mysqli_connect("localhost","root","ktx586247931", "db" );  
+if (!$link)  
+{  
+    echo "MySQL 접속 에러 : ";
+    echo mysqli_connect_error();
+    exit();  
+}  
+
+mysqli_set_charset($link,"utf8"); 
+
+
+$sql="select * from db.Person";
+
+$result=mysqli_query($link,$sql);
+$data = array();   
+if($result){  
+    
+    while($row=mysqli_fetch_array($result)){
+        array_push($data, 
+            array('id'=>$row[0],
+            'name'=>$row[1],
+            'address'=>$row[2]
+        ));
+    }
+
+    header('Content-Type: application/json; charset=utf8');
+    $json = json_encode(array("webnautes"=>$data), JSON_PRETTY_PRINT+JSON_UNESCAPED_UNICODE);
+    echo $json;
+}  
+else{  
+    echo "SQL문 처리중 에러 발생 : "; 
+    echo mysqli_error($link);
+} 
+
+
+ 
+mysqli_close($link);  
+   
+?>
